ebe4e349905e0cdc934384c72e31e82b51318189
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-16 16:57:40 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #!/bin/bash
-int main(void)
+int main()
 {
-	printf("with proper grammar, but the outcome is a piece of art,");	
+	printf("with proper grammar, but the outcome is a piece of art,\n");	
 	return(0);
 }
