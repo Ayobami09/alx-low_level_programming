39b3083cd8132d4c5cdda4597c2dafd0e4707798
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-16 17:45:12 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #!/bin/bash
-#include <studio.h> 
+#include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
