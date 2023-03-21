b231640a192b785f808821be5b54129c5b437a07
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-20 21:51:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * Description: Prints the lowercase alphabet
+ * using only the putchar function twice.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
