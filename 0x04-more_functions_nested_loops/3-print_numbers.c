41bfec0af9a4bbd4d18d9652a06dc2c7deb6c6a9
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-23 06:30:43 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+/**
+ * _putchar - adds a new line
+ *
+ */
