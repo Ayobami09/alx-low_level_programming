c330152d2f51c092f6cd7ab4edae9c1e865aac3e
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-23 05:48:51 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: checks for integer type input
+ * @b: checks for input
+ *
+ * Return: 0
+ *
+ */
+
+int mul(int a, int b)
+{
+	int prod = a * b;
+
+	return (prod);
+}
