e4a32edc3e1a8a2a8fcc117c1ff72d3801b634cf
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-24 03:52:13 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,21 +10,22 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
-	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		for (j = 0; j < 1; j++)
+		for (j = 1; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\\');
+		_putchar(92);
 		_putchar('\n');
 	}
 }
