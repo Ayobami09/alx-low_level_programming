1f9dca95bbc9af0ea28116e3950780153723f927
Fasasi Abd-Ahmid Oluwajuwon <f.ayobami09@gmail.com>
2023-03-28 16:41:00 -0700

    Task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
 
-import random
-import string
-
-valid_chars = string.ascii_letters + string.digits + '_-!@#$%^&*()'
-
-
-password_length = 10
-
-
-def generate_password():
-	password = ''.join(random.choice(valid_chars) for i in range(password_length))
-	return password
-
-def is_valid_password(password):
-    # TODO: Implement the password checking algorithm
-    pass
-
-password = generate_password()
-while not is_valid_password(password):
-	password = generate_password()
-
-print(password)
-
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
